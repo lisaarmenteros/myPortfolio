262dd4f117b152812bb8be9417fe29381bed7f28
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-29 17:14:35 -0500

    first commit

---
+++ src/client.js
@@ -0,0 +1,8 @@
+// Utilized to connect to the sanity client 
+
+import { sanityClient } from "@sanity/client";
+
+export default sanityClient({
+    projectId: "stcz81fx",
+    dataset: "production"
+})
